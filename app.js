aa2280777479052b6a4535b5d2b39413b08aa84a
Gururaj Dharani <dharani.gururaj@gmail.com>
2019-08-03 15:53:18 +0530

    add: conditions for colors -for 6 and 3 to both background and theme colors

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const utils = require('./utils/utils.js');
 const helpers = require('./helpers/helpers.js');
 // helper pointers
 const questions = utils.questions;
-const service_worker_config = helpers.service_worker_config;
+const sample_service_worker_config = helpers.service_worker_config;
+const sample_manifest = helpers.manifest;
 
-console.log('logged', service_worker_config);
-// inquirer.prompt(questions).then(answers => {
-//     console.log('ans', answers);
-// });
+inquirer.prompt(questions).then(answers => {
+    if(answers.framework == 'angular'){
+      sample_manifest.name = answers.appname;
+      sample_manifest.short_name = answers.appname;
+      sample_manifest.theme_color = answers.theme_color;
+      sample_manifest.background_color = answers.background_color;
+      sample_manifest.display = answers.display;
+      console.log('Your manifest file \n ----------------------------- \n', sample_manifest);
+    }
+    else {
+      console.log('currently supporting Angular 2+')
+    }
+});
--- utils/utils.js
+++ utils/utils.js
@@ -12,16 +12,32 @@ const questions = [{
   }
 },{
     type: 'input',
-    name: 'project',
-    message: 'Enter your project name?'
+    name: 'appname',
+    message: 'Enter your App name?'
   }, {
     type: 'input',
     name: 'theme_color',
-    message: 'Enter primary brand color?'
+    message: 'Enter primary brand color?',
+    prefix: '#',
+    validate: function(text) {
+      if(text.length == 6 || text.length == 3) {
+        return true;
+      } else {
+        return 'Invalid hex code';
+      }
+    }
   }, {
     type: 'input',
     name: 'background_color',
-    message: 'Enter background color? ( background for splash screen )'
+    message: 'Enter background color? ( background for splash screen )',
+    prefix: '#',
+    validate: function(text) {
+      if(text.length == 6 || text.length == 3) {
+        return true;
+      } else {
+        return 'Invalid hex code';
+      }
+    }
   }, {
     type: 'list',
     name: 'display',
